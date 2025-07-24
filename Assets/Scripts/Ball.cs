7579dfb2cd94952fc67edb247e786dd7258e9b80
JamesFletcher00 <123896424+JamesFletcher00@users.noreply.github.com>
2025-07-24 12:34:28 +0100

    Added Ball Logic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,10 +2,30 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D rb;
+    public float launchTimer = 2f;
+    public float maxY = 3f;
+    public float minY = -3;
+    public float ballSpeed = 5f;
+    public bool isLaunched = false;
+
     void Start()
     {
-        
+        rb.linearVelocity = Vector2.zero;
+        Invoke("LaunchBall", launchTimer);
+    }
+
+    void LaunchBall()
+    {
+        if (isLaunched) return;
+
+        int direction = Random.Range(0, 2) == 0 ? 1 : -1;
+        float randomY = Random.Range(minY, maxY);
+
+        Vector2 launchVelocity = new Vector2(direction * ballSpeed, randomY);
+        rb.linearVelocity = launchVelocity;
+
+        isLaunched = true;
     }
 
     // Update is called once per frame
